b5fc1b5639e3c88f5948a06cb870f32be5216897
Julie <julie.habbestad@hotmail.com>
2025-02-26 16:27:13 +0100

    footer styling

---
+++ src/components/footer/footer.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+   background-color: #333;
+   color: white;
+   text-align: center;
+   padding: 20px 0;
+   margin-top: 20px;
+`;
