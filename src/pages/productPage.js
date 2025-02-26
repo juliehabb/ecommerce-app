967f3f023a6f1f3030f2c6e4e7606b7b28544116
Julie <julie.habbestad@hotmail.com>
2025-02-26 16:28:46 +0100

    basic product page

---
+++ src/pages/productPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductPage = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Products</h1>
+      <p>Browse our high-quality survival and tactical gear.</p>
+    </div>
+  );
+};
+
+export default ProductPage;
