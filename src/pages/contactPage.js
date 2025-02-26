c8ad27ff34b6443eac20d2a7f765db7994fa6ded
Julie <julie.habbestad@hotmail.com>
2025-02-26 16:28:30 +0100

    basic contact page

---
+++ src/pages/contactPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      <p>Get in touch with us for any inquiries.</p>
+    </div>
+  );
+};
+
+export default ContactPage;
