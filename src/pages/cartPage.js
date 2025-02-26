a7e1df977a429941d6d543f5606107f49d7d601f
Julie <julie.habbestad@hotmail.com>
2025-02-26 16:28:05 +0100

    basic cart page

---
+++ src/pages/cartPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Shopping Cart</h1>
+      <p>Your selected items will appear here.</p>
+    </div>
+  );
+};
+
+export default CartPage;
