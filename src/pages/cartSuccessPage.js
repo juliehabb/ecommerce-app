3c9627af771c28a1b0af2b5888637e02dd098b3e
Julie <julie.habbestad@hotmail.com>
2025-03-05 19:46:48 +0100

    added basic functionality

--- src/pages/cartSuccessPage.js
+++ src/pages/cartSuccessPage.js
@@ -1,15 +1,24 @@
-import React from "react";
-import { SuccessContainer, SuccessTitle, SuccessMessage, BackToHomeButton } from "../styling/general";
+import React, { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { CartContext } from "../functionality/cartContext"; 
+import { SuccessContainer, SuccessMessage, BackToHomeButton } from "../styling/general";
 
 const CartSuccessPage = () => {
+  const { setCart } = useContext(CartContext); 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setCart([]); 
+    localStorage.setItem("cart", JSON.stringify([])); 
+  }, [setCart]);
 
   return (
     <SuccessContainer>
-      <SuccessTitle>Success!</SuccessTitle>
-      <SuccessMessage>The purchase was a success! You will get a confirmation email shortly.</SuccessMessage>
-      <BackToHomeButton onClick={() => navigate("/")}>Back to Home</BackToHomeButton>
+      <SuccessMessage>
+        <h1>Success!</h1>
+        <p>The purchase was a success! You will receive a confirmation email shortly.</p>
+        <BackToHomeButton onClick={() => navigate("/")}>Back to Home</BackToHomeButton>
+      </SuccessMessage>
     </SuccessContainer>
   );
 };
