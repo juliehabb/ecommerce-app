a7daeed41ffe9c7b0d544b01823245c2303f3f80
Julie <julie.habbestad@hotmail.com>
2025-03-09 12:29:58 +0100

    Added pagecontainer and wrapper

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header/header.js";
-import Footer from "./components/footer/footer.js";
-import HomePage from "./pages/homepage.js";
-import ProductPage from "./pages/productPage.js";
-import CartPage from "./pages/cartPage.js";
-import CartSuccessPage from "./pages/cartSuccessPage.js";
-import ContactPage from "./pages/contactPage.js";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import HomePage from "./pages/homepage";
+import ProductPage from "./pages/productPage";
+import CartPage from "./pages/cartPage";
+import CartSuccessPage from "./pages/cartSuccessPage";
+import ContactPage from "./pages/contactPage";
 import { CartProvider } from "./functionality/cartContext.js"; 
+import { PageContainer, ContentWrapper } from "./styling/layout.js"; 
 
 const App = () => {
     return (
-        <CartProvider> {}
+        <CartProvider>
             <Router>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/products/:id" element={<ProductPage />} />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route path="/cart-success" element={<CartSuccessPage />} />
-                    <Route path="/contact" element={<ContactPage />} />
-                </Routes>
-                <Footer />
+                <PageContainer>
+                    <Header />
+                    <ContentWrapper>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/products/:id" element={<ProductPage />} />
+                            <Route path="/cart" element={<CartPage />} />
+                            <Route path="/cart-success" element={<CartSuccessPage />} />
+                            <Route path="/contact" element={<ContactPage />} />
+                        </Routes>
+                    </ContentWrapper>
+                    <Footer />
+                </PageContainer>
             </Router>
         </CartProvider>
     );
