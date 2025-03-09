b0e194fac3635dc3a451cbe3fd743250f780863e
Julie <julie.habbestad@hotmail.com>
2025-03-09 12:59:59 +0100

    added layout component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
+import Layout from "./components/layout.js";
 import HomePage from "./pages/homepage";
 import ProductPage from "./pages/productPage";
 import CartPage from "./pages/cartPage";
 import CartSuccessPage from "./pages/cartSuccessPage";
 import ContactPage from "./pages/contactPage";
 import { CartProvider } from "./functionality/cartContext.js"; 
-import { PageContainer, ContentWrapper } from "./styling/layout.js"; 
 
 const App = () => {
     return (
         <CartProvider>
             <Router>
-                <PageContainer>
-                    <Header />
-                    <ContentWrapper>
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/products/:id" element={<ProductPage />} />
-                            <Route path="/cart" element={<CartPage />} />
-                            <Route path="/cart-success" element={<CartSuccessPage />} />
-                            <Route path="/contact" element={<ContactPage />} />
-                        </Routes>
-                    </ContentWrapper>
-                    <Footer />
-                </PageContainer>
+                <Layout> 
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/products/:id" element={<ProductPage />} />
+                        <Route path="/cart" element={<CartPage />} />
+                        <Route path="/cart-success" element={<CartSuccessPage />} />
+                        <Route path="/contact" element={<ContactPage />} />
+                    </Routes>
+                </Layout>
             </Router>
         </CartProvider>
     );
