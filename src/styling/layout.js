6b125d2306f2a4a5ed54923a9a130330653d12ee
Julie <julie.habbestad@hotmail.com>
2025-03-09 12:30:13 +0100

    added pagecontainer and contentwrapper

---
+++ src/styling/layout.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+   min-height: 100vh; /
+`;
+
+export const ContentWrapper = styled.div`
+   flex: 1;
+   padding-top: 4rem;
+   padding-bottom: 60px; 
+`;
